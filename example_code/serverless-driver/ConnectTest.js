29324973b6e05c1f52e0015b0591695ad0bb2971
shiyuhang <1136742008@qq.com>
2023-09-20 13:29:35 +0800

    remove fetch

--- example_code/serverless-driver/ConnectTest.js
+++ example_code/serverless-driver/ConnectTest.js
@@ -1,7 +1,6 @@
 import { connect } from '@tidbcloud/serverless'
-import { fetch } from 'undici'
 
 
-const conn = connect({url: process.env.DATABASE_URL,fetch:fetch})
-const result = await conn.execute("show database",null,{fullResult: true})
+const conn = connect({url: process.env.DATABASE_URL})
+const result = await conn.execute("show databases",null,{fullResult: true})
 console.log(result)
