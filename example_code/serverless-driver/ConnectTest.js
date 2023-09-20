86e7ad31fdfd0e0ff6c89168a4fb90e760cfb32b
shiyuhang <1136742008@qq.com>
2023-09-20 13:24:13 +0800

    test it

--- example_code/serverless-driver/ConnectTest.js
+++ example_code/serverless-driver/ConnectTest.js
@@ -4,4 +4,5 @@ import { fetch } from 'undici'
 
 const conn = connect({url: process.env.DATABASE_URL,fetch:fetch})
 const result = await conn.execute("show tables")
+console.log(process.env.DATABASE_URL)
 console.log(result)
