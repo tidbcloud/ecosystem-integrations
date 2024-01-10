d4c8ad2bb7a5d819509b3bd431a2c30937bee690
shiyuhang <1136742008@qq.com>
2024-01-10 11:54:47 +0800

    debug

--- example_code/serverless-driver/ConnectTest.js
+++ example_code/serverless-driver/ConnectTest.js
@@ -1,5 +1,5 @@
 import { connect } from '@tidbcloud/serverless'
 
-const conn = connect({url: process.env.DATABASE_URL})
+const conn = connect({url: process.env.DATABASE_URL,debug: true})
 const result = await conn.execute("show databases",null,{fullResult: true})
 console.log(result)
